d38af73ed849840beabbb5e03d55c2007da68194
Laith Al-Zou'bi <laithdev97@gmail.com>
2020-06-07 03:57:30 +0300

    Update CircularArrayQueue.java

--- src/CircularArrayQueue.java
+++ src/CircularArrayQueue.java
@@ -3,7 +3,7 @@ public class CircularArrayQueue<E> implements Queue<E> {
     private final E[] data;     // generic array used for storage
     private int front;          // index of the front element
     private int rear;           // index of the last element
-    private static int size;    // current number of elements
+    private static int size;    // number of filles cells in the array
 
     public CircularArrayQueue(int capacity) {
         front = -1;
